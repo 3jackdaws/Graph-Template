698dfe6c87716b9690aa64ac30a8a652cacf2861
Ian Murphy <immur.50ae@gmail.com>
2016-02-29 10:31:35 -0800

    Very late initial commit.  Pretty much done at this point.

---
+++ Vertex.h
@@ -0,0 +1,155 @@
+//
+//  Vertex.h
+//  Graph
+//
+//  Created by Ian Murphy on 2/27/16.
+//  Copyright © 2016 Ian Murphy. All rights reserved.
+//
+
+#ifndef Vertex_h
+#define Vertex_h
+
+#include "Edge.h"
+
+template <typename V, typename E>
+class Vertex
+{
+public:
+    Vertex();
+    Vertex(V data);
+    Vertex(const Vertex & cp);
+    Vertex<V,E> & operator = (const Vertex<V,E> & cp);
+    
+    V GetData();
+    void SetData(V);
+    bool IsProcessed();
+    void SetProcessed(bool);
+    void AddEdge(E, Vertex<V,E> *,double);
+    bool RemoveEdge(E);
+    
+    void Begin();
+    void MoveNext();
+    //void GetPrev();
+    bool IsDone();
+    Edge<V,E> & GetEdge();
+    bool & Processed();
+
+private:
+    
+    V _data;
+    bool _processed;
+    std::list<Edge<V, E>> _edges;
+    typename std::list<Edge<V, E>>::iterator _internal;
+    
+    
+};
+
+template <typename V, typename E>
+Vertex<V, E>::Vertex() : _processed(false)
+{
+    
+}
+
+template <typename V, typename E>
+Vertex<V, E>::Vertex(V data) : _processed(false), _data(data)
+{
+    
+}
+
+template <typename V, typename E>
+Vertex<V, E>::Vertex(const Vertex & cp) : _processed(cp._processed), _edges(cp._edges), _data(cp._data)
+{
+    
+}
+
+template <typename V, typename E>
+Vertex<V,E> & Vertex<V, E>::operator=(const Vertex<V, E> & rhs)
+{
+    if(this != &rhs)
+    {
+        _data = rhs._data;
+        _processed = rhs._processed;
+        _edges = rhs._edges;
+    }
+    return *this;
+}
+
+template <typename V, typename E>
+V Vertex<V, E>::GetData()
+{
+    return _data;
+}
+
+template <typename V, typename E>
+void Vertex<V, E>::AddEdge(E data, Vertex<V,E> * link, double weight)
+{
+    _edges.emplace_back(data, link, weight);
+}
+
+template <typename V, typename E>
+bool Vertex<V, E>::RemoveEdge(E data)
+{
+    typename std::list<Edge<V,E>>::iterator iter = _edges.begin();
+    bool found = false;
+    bool found_other = false;
+    while(iter != _edges.end() && !found)
+    {
+        if(iter->GetData() == data)
+        {
+            found = true;
+            
+            iter->GetLink().Begin();
+            
+            while(!iter->GetLink().IsDone() && !found_other)
+            {
+                if(iter->GetLink().GetEdge().GetData() == data)
+                {
+                    iter->GetLink()._edges.erase(iter->GetLink()._internal);
+                    found_other = true;
+                }
+                iter->GetLink().MoveNext();
+            }
+            _edges.erase(iter);
+        }
+        iter++;
+    }
+    if(found)
+        return true;
+    return false;
+}
+
+template <typename V, typename E>
+void Vertex<V, E>::Begin()
+{
+    _internal = _edges.begin();
+}
+
+template <typename V, typename E>
+void Vertex<V, E>::MoveNext()
+{
+    _internal++;
+}
+
+
+template <typename V, typename E>
+Edge<V, E> & Vertex<V, E>::GetEdge()
+{
+    return *_internal;
+}
+
+template <typename V, typename E>
+bool Vertex<V, E>::IsDone()
+{
+    if(_internal == _edges.end())
+    {
+        return true;
+    }
+    return false;
+}
+
+template <typename V, typename E>
+bool & Vertex<V, E>::Processed()
+{
+    return _processed;
+}
+#endif /* Vertex_h */
---
+++ main.cpp
@@ -0,0 +1,138 @@
+//
+//  main.cpp
+//  Graph
+//
+//  Created by Ian Murphy on 2/27/16.
+//  Copyright © 2016 Ian Murphy. All rights reserved.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "Graph.h"
+
+void Print(string data)
+{
+    cout<<data<<endl;
+}
+
+bool Error(string emsg)
+{
+    cout<<"Error: " + emsg << endl;
+    return true;
+}
+
+int main(int argc, const char * argv[]) {
+    
+//    Graph<string, string> mygraph;
+//    cout<<"===   Add Vertices   ==="<<endl;
+//    mygraph.AddVertex("Troutdale");
+//    mygraph.AddVertex("Gresham");
+//    mygraph.AddVertex("Portland");
+//    mygraph.AddVertex("Sandy");
+//    mygraph.AddVertex("Fairview");
+//    mygraph.AddVertex("Rockwood");
+//    mygraph.AddVertex("Welches");
+//    
+//    
+//    
+//    cout<<"===   Add Edges   ==="<<endl;
+//    mygraph.AddEdge("Troutdale", "Gresham", "Road", 10) || cout<<mygraph.GetError();
+//    mygraph.AddEdge("Troutdale", "Sandy", "Orient Drive", 20) || cout<<mygraph.GetError();
+//    mygraph.AddEdge("Troutdale", "Portland", "Stark Street", 23) || cout<<mygraph.GetError();
+//    mygraph.AddEdge("Gresham", "Rockwood", "Stark Street", 5) || cout<<mygraph.GetError();
+//    mygraph.AddEdge("Troutdale", "Fairview", "257th Drive", 6) || cout<<mygraph.GetError();
+//    mygraph.AddEdge("Gresham", "rprpf", "Stark Street", 23) || cout<<mygraph.GetError();
+//    
+//    for(mygraph.Begin(); !mygraph.IsDone(); mygraph.MoveNext())
+//    {
+//        cout<<mygraph.GetVertex().GetData()<<endl;
+//        mygraph.GetVertex().Begin();
+//        for (mygraph.GetVertex().Begin(); !mygraph.GetVertex().IsDone(); mygraph.GetVertex().MoveNext())
+//        {
+//            cout<<mygraph.GetVertex().GetEdge().GetData()<< " to " << mygraph.GetVertex().GetEdge().GetLink().GetData() <<
+//            ", Length: "<< mygraph.GetVertex().GetEdge().GetWeight() << endl;
+//        }
+//    }
+//    
+//    Graph<string, string> graph2(mygraph);
+//    mygraph.DeleteVertex("Troutdale");
+//    for(graph2.Begin(); !graph2.IsDone(); graph2.MoveNext())
+//    {
+//        cout<<graph2.GetVertex().GetData()<<endl;
+//        graph2.GetVertex().Begin();
+//        for (graph2.GetVertex().Begin(); !graph2.GetVertex().IsDone(); graph2.GetVertex().MoveNext())
+//        {
+//            cout<<graph2.GetVertex().GetEdge().GetData()<< " to " << graph2.GetVertex().GetEdge().GetLink().GetData() <<
+//            ", Length: "<< graph2.GetVertex().GetEdge().GetWeight() << endl;
+//        }
+//    }
+
+    cout<<" = = = = = INT Testing = = = = = "<<endl;
+    
+    Graph<int, string> intGraph;
+    for (int i = 0; i<200; i++)
+    {
+        intGraph.AddVertex(i);
+    }
+    srand(time(NULL));
+    for (int i = 0; i<100; i++)
+    {
+        intGraph.AddEdge(rand()%200, rand()%200, "Path", (rand()%1000)/2.3);
+    }
+    for(intGraph.Begin(); !intGraph.IsDone(); intGraph.MoveNext())
+    {
+        cout<<intGraph.GetVertex().GetData()<<endl;
+        intGraph.GetVertex().Begin();
+        for (intGraph.GetVertex().Begin(); !intGraph.GetVertex().IsDone(); intGraph.GetVertex().MoveNext())
+        {
+            cout<<"\t"<<intGraph.GetVertex().GetEdge().GetData()<< " to " << intGraph.GetVertex().GetEdge().GetLink().GetData() <<
+            ", Length: "<< intGraph.GetVertex().GetEdge().GetWeight() << endl;
+        }
+    }
+    
+    
+    
+    
+//    for (intGraph.Begin(); !intGraph.IsDone(); intGraph.MoveNext())
+//    {
+//        cout<<intGraph.GetVertex().GetData()<<endl;
+//    }
+//    
+//    Graph<int, int> intGraph2(intGraph);
+//    for (int i = 5; i<490; i++)
+//    {
+//        if(i>30)
+//            intGraph2.DeleteVertex(i) || cout<<intGraph2.GetError();
+//    }
+//    for (intGraph2.Begin(); !intGraph2.IsDone(); intGraph2.MoveNext())
+//    {
+//        cout<<intGraph2.GetVertex().GetData()<<endl;
+//    }
+//    
+//    mygraph.AddVertex("Test");
+//    mygraph.AddVertex("Test 2");
+//    mygraph.DeleteVertex("Test");
+//    mygraph.AddEdge("Test", "Test 2", "Testerino", 2.4);
+//    mygraph.DeleteEdge("Test", "Test 2", "Testerino");
+//    mygraph.DepthFirst(&Print);
+//    mygraph.BreadthFirst(&Print);
+//    mygraph.isEmpty();
+//    
+//    mygraph.Begin();
+//    mygraph.End();
+//    mygraph.GetVertex();
+//    mygraph.MoveNext();
+//    
+//    mygraph.IsDone();
+//    
+//    mygraph.GetError();
+//    
+//    mygraph.BreadthFirst(&Print);
+//    
+//    Vertex<string, string> testVertex;
+//    Vertex<string, string> testVertex2("Hi there");
+//    testVertex = testVertex2;
+//    cout<<testVertex.GetData()<<endl;
+    return 0;
+}
